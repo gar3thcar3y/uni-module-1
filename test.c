f435e20be2ae8ee65b923aeb755eb91fa1304fb4
gar3thcar3y <168173753+gar3thcar3y@users.noreply.github.com>
2024-10-18 12:01:19 +0100

    added function for resisters in parallel calculation

--- test.c
+++ test.c
@@ -1,4 +1,14 @@
 #include <stdio.h>
+#include <string.h>
+#include <limits.h> // For INT_MAX
+
+float rtotal(float r1, float r2, float r3){
+    if (r1 == 0 || r2 == 0 || r3 == 0) {
+        printf("Error: Resistance values must be greater than zero.\n");
+        return -1; // Indicate an error
+    }
+    return 1/(1/r1 + 1/r2 + 1/r3);
+}
 
 int main() {
     float num = 0.111;
@@ -15,7 +25,17 @@ int main() {
     for(int i=0; i<sizeof(arr)/sizeof(arr[0]); i++){
         printf("%d",arr[i]); printf(",");
     }
-    printf("}");
+    printf("}\n");
 
-    return 0;
+
+    char txt[] = "We are the so-called \"Vikings\" from the north.";
+    printf(txt);
+    
+    char txt1[100] = "hello";
+    char txt2[] = " world";
+    strcat(txt1, txt2);
+    printf("\n%s",txt1);
+
+    printf("\n%f", rtotal(1, 2, 3));
+
 }

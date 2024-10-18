649dfd729e6b06a00ff4e3490e57ca8ba14dd462
gar3thcar3y <168173753+gar3thcar3y@users.noreply.github.com>
2024-10-18 11:25:15 +0100

    testing arrays

--- test.c
+++ test.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
 
+int main() {
+    float num = 0.111;
+    int arr[] = {1, 2, 3, 4, 5};
+    printf("Hello World!");
+    printf("\n%s","num=");
+    printf("%f", num);
+    printf("\n%d", arr[1]);
+    printf("\n%lu", sizeof(arr));
+    printf("\n%lu", sizeof(arr[0]));
+    printf("\n%lu", sizeof(num));
+
+    return 0;
+}
+
+void printarray(&arr){
+    printf("\n{");
+    for(int i=0; i<sizeof(arr)/sizeof(arr[0]); i++){
+        printf("%d",arr[i]); printf(",");
+    }
+    printf("}");
+}

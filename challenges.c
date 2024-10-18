be2e5bfc3fe99a732d6862ed3a8bc13c477fb3ec
gar3thcar3y <168173753+gar3thcar3y@users.noreply.github.com>
2024-10-18 20:30:18 +0100

    added products structs and methods

--- challenges.c
+++ challenges.c
@@ -1,7 +1,35 @@
 #include <stdio.h>
 
+struct product {
+    int ID;
+    char name[16];
+    int quantity;
+    float price;
+};
+
+
+struct products {
+    int size;
+    struct product array[20];
+};
+
+void products_init(struct products* Products){
+    Products->size = 0;
+}
+
+void add_product(struct products* Products, struct product Product){
+    Products->array[Products->size] = Product;
+    Products->size = Products->size + 1;
+}
 
 int main(){
+    struct product products[20];
     printf("hello world");
+    struct product pineapple;
+    pineapple.ID = 2;
+    printf("\n%d", pineapple.ID);
+    struct products PRODUCTS;
+    products_init(&PRODUCTS);
+    add_product(&PRODUCTS, pineapple);
     return 0;
 }

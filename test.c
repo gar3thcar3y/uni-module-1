cbed4faf32ab460240d0147e191dc9b293696dbf
gar3thcar3y <168173753+gar3thcar3y@users.noreply.github.com>
2024-10-18 12:14:37 +0100

    added input andd sleep

--- test.c
+++ test.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <string.h>
 #include <limits.h> // For INT_MAX
+#include <unistd.h>
 
 float rtotal(float r1, float r2, float r3){
     if (r1 == 0 || r2 == 0 || r3 == 0) {
         printf("Error: Resistance values must be greater than zero.\n");
-        return -1; // Indicate an error
+        return -1;
     }
     return 1/(1/r1 + 1/r2 + 1/r3);
 }
@@ -38,4 +39,20 @@ int main() {
 
     printf("\n%f", rtotal(1, 2, 3));
 
+
+    // Create an integer variable that will store the number we get from the user
+    int myNum;
+
+    // Ask the user to type a number
+    printf("Type a number: \n");
+
+    // Get and save the number the user types
+    scanf("%d", &myNum);
+
+    // Output the number the user typed
+    printf("Your number is: %d", myNum);
+
+    sleep(5);
+
+    return 0;
 }
